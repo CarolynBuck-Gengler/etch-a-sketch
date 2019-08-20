4486696ebb186a3c6414983864f783669dac82f1
Carolyn Buck-Gengler <carolyn.buckgengler@gmail.com>
2019-08-20 17:29:55 -0600

    Add "hover" effect with javascript event listeners

--- script.js
+++ script.js
@@ -2,9 +2,39 @@ const numSquares = 16;
 let backgroundColor = 'white';
 
 
-
 function main() {
+    // set up grid
+    
+    createGrid();
+    const cells = document.querySelectorAll('.cell');
 
+    // alert("about to do event listener part");
+    // alert(cells);
+    // console.log(cells);
+    cells.forEach((div) => {
+        div.addEventListener('mouseenter', (e) => {
+            div.style.backgroundColor = 'red';
+        });
+        div.addEventListener('mouseleave', (e) => {
+            div.style.backgroundColor = 'pink';
+        });
+        // div.addEventListener('click', (e) => {
+        //     console.log(e);
+        //     alert("click on cell ");
+        // });
+    });
+
+
+
+
+}
+
+function toggleBgColor(curColor) {
+    // mainly for testing creation of grid
+    return (curColor === 'white'?'rgb(243, 243, 243)':'white');
+}
+
+function createGrid() {
     const gridloc = document.querySelector(".etch-a-sketch-grid");
 
     for (i=0;i<numSquares;i++) {
@@ -21,19 +51,7 @@ function main() {
             gridloc.appendChild(coldiv);
         }
 
-        // gridloc.appendChild(rowdiv);
-        // backgroundColor = toggleBgColor(backgroundColor);
-
-
     }
-
-
-
-}
-
-function toggleBgColor(curColor) {
-    // mainly for testing creation of grid
-    return (curColor === 'white'?'rgb(243, 243, 243)':'white');
 }
 
 
